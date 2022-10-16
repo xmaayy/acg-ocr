22eccd77b8f348e1704540353bcf0afeb143a6c1
xmaayy <xanderm@justappraised.com>
2022-10-16 14:37:11 -0400

    New automation stuff

---
+++ scripts/first_deployment.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd /home/ec2-user/acg-ocr
+sudo cp nginx.conf /etc/nginx/nginx.conf
+python3 -m pip install -r /home/ec2-user/acg-ocr/requirements.txt
+
+# Create and Enable the Service
+sudo cp acg-ocr.service /etc/systemd/system/acg-ocr.service
+sudo systemctl daemon-reload
+sudo systemctl start acg-ocr.service
+
+# Get the webserver pointing at the new service
+sudo systemctl restart nginx
